a42cee88d5eff959bab45e058686f15dff82dc1e
kashiling <58911530+kashiling@users.noreply.github.com>
2019-12-25 17:55:22 +0530

    Add project files.

---
+++ AutomationAll/Login.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Configuration;
+
+namespace AutomationAll
+{
+    public class Login
+    {
+
+        [Test]
+        public void check()
+        {
+            Console.WriteLine("Hi");
+            IWebDriver driver = new ChromeDriver();
+            var url = ConfigurationSettings.AppSettings["URL"];
+            driver.Url = url;
+            driver.Quit();
+        }
+    }
+}
