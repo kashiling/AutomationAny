bf7c3df0ccf89a787484c909cdd05a974ebf9dfb
kashiling <58911530+kashiling@users.noreply.github.com>
2019-12-29 00:06:24 +0530

    Added code for APMC site : Pomogranate rate check

--- AutomationAll/Login.cs
+++ AutomationAll/Login.cs
@@ -3,6 +3,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Configuration;
+using System.Linq;
 
 namespace AutomationAll
 {
@@ -10,14 +11,31 @@ namespace AutomationAll
     {
 
         [Test]
-        public void check()
+        public void CheckPomogranateRate()
         {
-            Console.WriteLine("Hi");
             IWebDriver driver = new ChromeDriver();
             var url = ConfigurationSettings.AppSettings["URL"];
             driver.Url = url;
 
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(120);
+            driver.Manage().Window.Maximize();
+            var title = driver.Title;
+            Assert.AreEqual("कृषि उत्पन्न बाजार समिती, पुणे", title);
+            IWebElement DateLink = driver.FindElement(By.XPath("//*[@id='form1']/table/tbody/tr[4]/td/ul[2]/li[2]/a"));
+            DateLink.Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(120);
+            driver.SwitchTo().Window(driver.WindowHandles.Last());
+            IWebElement GetPomoogranteRowNumbe = driver.FindElement(By.XPath("//*[@id='form1']/table[4]/tbody/tr[100]/td[1]/strong"));
+            System.Threading.Thread.Sleep(2000);
+            var rownumber = GetPomoogranteRowNumbe.Text;
+            Assert.AreEqual("4114", rownumber);
+          
+
             driver.Quit();
         }
+    }
+
+    internal class thread
+    {
     }
 }
